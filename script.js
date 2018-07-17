96b8ae66d539818cec0362124ff96f9ca4ca3cd7
Makjuho <37654702+MakJuho@users.noreply.github.com>
2018-07-17 18:43:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -280,13 +280,13 @@
         }
         // x축에 대한 가속도 데시벨
      
-        var result_x = 20 * Math.log10(abs(send_acc_x) * 100000);
-        var result_y = 20 * Math.log10(abs(send_acc_y) * 100000);
-        var result_z = 20 * Math.log10(abs(send_acc_z) * 100000);
+        var result_x = 20 * Math.log10(Math.abs(send_acc_x) * 100000);
+        var result_y = 20 * Math.log10(Math.abs(send_acc_y) * 100000);
+        var result_z = 20 * Math.log10(Math.abs(send_acc_z) * 100000);
 
-        // var final_val = (result_x + result_y + result_z) / 3
-        value.textContent = result_x;
-        data = result_x;
+        var final_val = (result_x + result_y + result_z) / 3
+        value.textContent = final_val;
+        data = final_val;
         // 출력부분
 
         // 값은 tmp_vibrate로 넘겨준다.
